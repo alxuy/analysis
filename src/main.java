bf25260941af8d0646f7daf5ad290bca7be1220a
Alexandre Uy <alexandre.uy@gmail.com>
2017-12-08 14:15:01 +0100

    get data from database

--- src/main.java
+++ src/main.java
@@ -4,7 +4,8 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.mongodb.client.model.Filters.eq;
 
@@ -24,6 +25,16 @@ public class main {
         MongoDatabase database = mongoClient.getDatabase("test");
         MongoCollection<Document> collection = database.getCollection("associations");
         Document document = collection.find(eq("name", "La Croix-rouge")).first();
-        System.out.println(document);
+        document = (Document) document.get("ulule");
+        ArrayList projects = (ArrayList) document.get("projects");
+        List<String> contents = new ArrayList<String>();
+        for (int i = 0; i < 4; i++) {
+            document = (Document) projects.get(i);
+            contents.add((String) document.get("content"));
+        }
+        for (int i = 0; i < 4; i++) {
+            System.out.println(i);
+            System.out.println(contents.get(i));
+        }
     }
 }
