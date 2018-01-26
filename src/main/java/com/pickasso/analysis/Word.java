b88aa96d923a5fd72e6f46ec82dae7ba09b0c28d
yedane <yedane123@gmail.com>
2018-01-26 12:36:51 +0100

    include numberDocContian

--- src/main/java/com/pickasso/analysis/Word.java
+++ src/main/java/com/pickasso/analysis/Word.java
@@ -1,32 +1,48 @@
 package com.pickasso.analysis;
 
 public class Word {
-	
+
 	private String word;
 	private Integer count;
-	
+	private Integer numberDocContain;
+
 	public Word(String word, int count) {
 		super();
 		this.word = word;
 		this.count = count;
 	}
-	
+
+	public Word(String word, int count, Integer numberDocCountain) {
+		super();
+		this.word = word;
+		this.count = count;
+		this.numberDocContain = numberDocCountain;
+	}
+
 	public Word() {
 	}
-	
-	public void setWord(String word){
+
+	public void setWord(String word) {
 		this.word = word;
 	}
-	
-	public String getWord(){
+
+	public String getWord() {
 		return this.word;
 	}
-	
-	public void setCount(int count){
-		this.count=count;
+
+	public void setCount(int count) {
+		this.count = count;
 	}
-	
-	public Integer getCount(){
+
+	public Integer getCount() {
 		return this.count;
+	}
+
+	public Integer getNumberDocContain() {
+		return numberDocContain;
+	}
+
+	public void setNumberDocContain(Integer numberDocContain) {
+		this.numberDocContain = numberDocContain;
 	}
 }
