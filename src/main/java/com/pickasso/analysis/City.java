5305df2c497ada3396c41347c5cd4e0782fb9f80
JinyuYUAN <jinyu.yuan@efrei.net>
2018-02-16 03:34:24 +0100

    city.json

--- src/main/java/com/pickasso/analysis/City.java
+++ src/main/java/com/pickasso/analysis/City.java
@@ -2,8 +2,10 @@ package com.pickasso.analysis;
 
 import static com.mongodb.client.model.Filters.eq;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -73,39 +75,72 @@ public class City {
 		Document carenews = (Document) document.get("carenews");
 		ArrayList<Document> articleList = (ArrayList<Document>) carenews.get("articles");
 
-		// String text = "des etudiants à Torcy et Précy-sur-Marne-tezji à
-		// Précy-sur-Marne et encore à Torcy et torcy";
-		// System.out.println("\n\nTEXT=" + text);
-
+		try {
+			FileWriter fw = new FileWriter("city.json", true);
+			fw.write("{\"associations\":{\"name\":\"" + associationName + "\",\"carenews\":{\"articles\":[");
+			fw.flush();
+			fw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		articleList.forEach(article -> {
 			String text = article.getString("content");
-			System.out.println(text.toString());
-			chunk(dictionaryChunkerTT, text);
+			String title = article.getString("title");
+			title = title.replaceAll("\"", "\\\\\"");
+			String str = "{\"title\":\"" + title + "\",\"city\":[";
+			str += chunk(dictionaryChunkerTT, text) + "]},";
 			chunk(dictionaryChunkerTF, text);
 			chunk(dictionaryChunkerFT, text);
 			chunk(dictionaryChunkerFF, text);
+
+			try {
+				FileWriter fw = new FileWriter("cifty.json", true);
+				fw.write(str);
+				fw.flush();
+				fw.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		});
 
+		try {
+			FileReader fr = new FileReader("city.json");
+			BufferedReader br = new BufferedReader(fr);
+			String s = br.readLine();
+			if ((s.substring(s.length() - 1, s.length())).equals(",")) {
+				s = s.substring(0, s.length() - 1);
+			}
+			fr.close();
+
+			FileWriter fw = new FileWriter("city.json", false);
+			fw.write(s + "]}}}");
+			fw.flush();
+			fw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	static void chunk(ExactDictionaryChunker chunker, String text) {
+	static String chunk(ExactDictionaryChunker chunker, String text) {
 		System.out.println("\nChunker." + " All matches=" + chunker.returnAllMatches() + " Case sensitive="
 				+ chunker.caseSensitive());
 		Chunking chunking = chunker.chunk(text);
 		GsonBuilder gsonBuilder = new GsonBuilder();
 		gsonBuilder.serializeSpecialFloatingPointValues();
 		Gson gson = gsonBuilder.create();
+		String str = "";
 		for (Chunk chunk : chunking.chunkSet()) {
 			int start = chunk.start();
 			int end = chunk.end();
 			String type = chunk.type();
 			double score = chunk.score();
 			String phrase = text.substring(start, end);
-			System.out.println(gson.toJson(chunk));
-			// System.out.println(" phrase=|" + phrase + "|" + " start=" + start + " end=" +
-			// end + " type=" + type
-			// + " score=" + score);
+			str += "{\"phrase\":\"" + phrase + "\",\"start\":" + start + ",\"end\":" + end + ",\"type\":\"" + type
+					+ "\",\"score\":\"" + score + "\"},";
 		}
+		if (!str.equals("")) {
+			str = str.substring(0, str.length() - 1);
+		}
+		return str;
 	}
-
 }
